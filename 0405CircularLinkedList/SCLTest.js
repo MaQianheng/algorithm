5f4180218384b5e7b10702d22fe4c257ead4851b
MaQianheng <894585638@qq.com>
2021-09-04 21:09:26 +0800

    update

--- 0405CircularLinkedList/SCLTest.js
+++ 0405CircularLinkedList/SCLTest.js
@@ -1,5 +1,11 @@
 import {SingleCircularLinkedList} from "./SingleCircularLinkedList.js"
 
 // const sclTmp = new SingleCircularLinkedList()
-const sclTmp = new SingleCircularLinkedList(Array(8).fill().map((_, numIndex) => numIndex))
+let sclTmp = new SingleCircularLinkedList(Array(8).fill().map((_, numIndex) => numIndex))
+// sclTmp.print()
+console.log(sclTmp.removeByIndex(5).anyData)
 sclTmp.print()
+/**
+ * Josephu（约瑟夫）问题
+ * 设编号为1，2...n的n个人围坐一圈，约定编号为k（1<=k<=n）的人总1开始报数，数到m的人出列，他的下一位又从1开始报数，数到m的人又出列，以此类推，直到所有人出列为止，由此产生一个出对编号的队列。
+ */
--- 0405CircularLinkedList/SingleCircularLinkedList.js
+++ 0405CircularLinkedList/SingleCircularLinkedList.js
@@ -28,6 +28,51 @@ export class SingleCircularLinkedList {
         }
     }
 
+    getNodeLength() {
+        let numRes = 1
+        let sclTmp = this
+        while (sclTmp.sclNex !== this.sclHead) {
+            numRes++
+            sclTmp = sclTmp.sclNex
+        }
+        return numRes
+    }
+
+    getNodeByIndex(numIndex) {
+        // positive: 正序
+        // negative: 倒序
+        let numCurTraverseCount = 0
+        let sclRes = this
+        if (numIndex >= 0) {
+            while (numCurTraverseCount !== numIndex) {
+                sclRes = sclRes.sclNex
+                numCurTraverseCount++
+                if (sclRes === this.sclHead && numCurTraverseCount <= numIndex) throw new Error(`Index out of range which max is ${numCurTraverseCount - 1}`)
+            }
+        } else {
+            const numTotalLength = this.getNodeLength()
+            if (-(numIndex) > numTotalLength) throw new Error(`Index out of range which min is ${-(numTotalLength)}`)
+            if (-(numIndex) === numTotalLength) return this.sclHead
+            while (numCurTraverseCount < (numTotalLength + numIndex)) {
+                sclRes = sclRes.sclNex
+                numCurTraverseCount++
+            }
+        }
+        return sclRes
+    }
+
+    removeByIndex(numIndex) {
+        // todo: support remove head node, if remove head node update all nodes' sclHead property
+        numIndex = numIndex - 1
+        if (numIndex === -1) throw new Error('Head node cannot be removed')
+        if (numIndex < 0) throw new Error('Index invalid')
+        const sclTmp = this.getNodeByIndex(numIndex)
+        if (sclTmp.sclNex === this.sclHead) throw new Error(`Index out of range`)
+        const sclRes = sclTmp.sclNex
+        sclTmp.sclNex = sclTmp.sclNex.sclNex
+        return sclRes
+    }
+
     print() {
         let sclHead = this.sclHead, sclCur = this
         do {
