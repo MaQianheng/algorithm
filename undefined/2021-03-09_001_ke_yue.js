9cd10fdebb884872137a4ea3ca315d3981fed1a7
MaQianheng <894585638@qq.com>
2023-09-29 08:57:29 +0800

    update

--- undefined/2021-03-09_001_ke_yue.js
+++ undefined/2021-03-09_001_ke_yue.js
@@ -1,26 +1,26 @@
 // (abc)de(fg)hi(jklm)n(o) -> cbadegfhimlkjno
 // 反转括号内的字符
 function func1(str) {
-    let arrStack = []
-    let strRes = ""
-    let ifIn = false
-    for (let i = 0; i < str.length; i++) {
-        let anyTmp = str[i]
-        switch (anyTmp) {
-            case "(":
-                ifIn = true
-                continue
-            case ")":
-                ifIn = false
-                strRes += arrStack.reverse().join("")
-                arrStack = []
-                continue
-            default:
-                ifIn ? arrStack.push(anyTmp) : strRes += anyTmp
-                break
-        }
+  let arrStack = [];
+  let strRes = '';
+  let ifIn = false;
+  for (let i = 0; i < str.length; i++) {
+    let anyTmp = str[i];
+    switch (anyTmp) {
+      case '(':
+        ifIn = true;
+        continue;
+      case ')':
+        ifIn = false;
+        strRes += arrStack.reverse().join('');
+        arrStack = [];
+        continue;
+      default:
+        ifIn ? arrStack.push(anyTmp) : (strRes += anyTmp);
+        break;
     }
-    return strRes
+  }
+  return strRes;
 }
 
 // console.log(func1("(abc)de(fg)hi(jklm)n(o)"))
@@ -28,90 +28,91 @@ function func1(str) {
 // [1,2,3,6,8,9,11], [3,6,8,11,12,16,23]
 // 按从小到大顺序合并两个已排序数组
 function func2(arr1, arr2) {
-    let arrRes = []
-    let numPointer1 = 0
-    let numPointer2 = 0
-    while (true) {
-        let num1 = arr1[numPointer1]
-        let num2 = arr2[numPointer2]
-        if (!num1) {
-            arrRes = arrRes.concat(arr2.slice(numPointer2))
-            break
-        }
-        if (!num2) {
-            arrRes = arrRes.concat(arr1.slice(numPointer1))
-            break
-        }
-        if (num1 < num2) {
-            arrRes.push(num1)
-            numPointer1 += 1
-        } else {
-            arrRes.push(num2)
-            numPointer2 += 1
-        }
+  let arrRes = [];
+  let numPointer1 = 0;
+  let numPointer2 = 0;
+  while (true) {
+    let num1 = arr1[numPointer1];
+    let num2 = arr2[numPointer2];
+    if (!num1) {
+      arrRes = arrRes.concat(arr2.slice(numPointer2));
+      break;
     }
-    return arrRes
+    if (!num2) {
+      arrRes = arrRes.concat(arr1.slice(numPointer1));
+      break;
+    }
+    if (num1 < num2) {
+      arrRes.push(num1);
+      numPointer1 += 1;
+    } else {
+      arrRes.push(num2);
+      numPointer2 += 1;
+    }
+  }
+  return arrRes;
 }
 
 // console.log(func2([1, 2, 3, 6, 8, 9, 11], [3, 6, 8, 11, 12, 16, 23]))
 // console.log([1, 2, 3, 6, 8, 9, 11].concat([3, 6, 8, 11, 12, 16, 23]).sort((a, b) => a - b))
 
 function funcFindSmallestValeIndex(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] !== '/') return i
-    }
-    return 0
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== '/') return i;
+  }
+  return 0;
 }
 
 // [[1,2,3,6,8,9,11], [3,6,8,11,12,16,23], [4,6,8,11,14]]
 function func3(arrTwoD) {
-    let numCounter = 0
+  let numCounter = 0;
 
-    let arrRes = []
-    let numArrLength = arrTwoD.length
-    let arrPointer = Array(numArrLength).fill(0)
+  let arrRes = [];
+  let numArrLength = arrTwoD.length;
+  let arrPointer = Array(numArrLength).fill(0);
 
-    let arrNumTmp = []
-    let numFinishedQueue = 0
-    while (true) {
-        let numSmallest = undefined
-        let numSmallestNumberIndex = 0
-        for (let i = 0; i < numArrLength; i++) {
-            numCounter++
-            if (arrNumTmp[i] === "/") continue
-            let arrTmp = arrTwoD[i]
-            if (!arrNumTmp[i]) {
-                let numPointer = arrPointer[i]
-                if (arrTmp[numPointer]) {
-                    arrNumTmp[i] = arrTmp[numPointer]
-                } else {
-                    arrNumTmp[i] = '/'
-                    numFinishedQueue++
-                    if (numFinishedQueue === numArrLength - 1) {
-                        let numLastArrIndex = funcFindSmallestValeIndex(arrNumTmp)
-                        arrRes = arrRes.concat(arrTwoD[numLastArrIndex].slice([arrPointer[numLastArrIndex]]))
-                        console.log(numCounter)
-                        return arrRes
-                    }
-                    continue
-                }
-            }
-            if (!numSmallest) {
-                numSmallest = arrNumTmp[i]
-                numSmallestNumberIndex = i
-            } else {
-                if (arrNumTmp[i] < numSmallest) {
-                    numSmallest = arrNumTmp[i]
-                    numSmallestNumberIndex = i
-                }
-            }
+  let arrNumTmp = [];
+  let numFinishedQueue = 0;
+  while (true) {
+    let numSmallest = undefined;
+    let numSmallestNumberIndex = 0;
+    for (let i = 0; i < numArrLength; i++) {
+      numCounter++;
+      if (arrNumTmp[i] === '/') continue;
+      let arrTmp = arrTwoD[i];
+      if (!arrNumTmp[i]) {
+        let numPointer = arrPointer[i];
+        if (arrTmp[numPointer]) {
+          arrNumTmp[i] = arrTmp[numPointer];
+        } else {
+          arrNumTmp[i] = '/';
+          numFinishedQueue++;
+          if (numFinishedQueue === numArrLength - 1) {
+            let numLastArrIndex = funcFindSmallestValeIndex(arrNumTmp);
+            arrRes = arrRes.concat(
+              arrTwoD[numLastArrIndex].slice([arrPointer[numLastArrIndex]])
+            );
+            console.log(numCounter);
+            return arrRes;
+          }
+          continue;
         }
-        arrNumTmp[numSmallestNumberIndex] = undefined
-        arrPointer[numSmallestNumberIndex]++
-        arrRes.push(numSmallest)
-        numSmallest = undefined
+      }
+      if (!numSmallest) {
+        numSmallest = arrNumTmp[i];
+        numSmallestNumberIndex = i;
+      } else {
+        if (arrNumTmp[i] < numSmallest) {
+          numSmallest = arrNumTmp[i];
+          numSmallestNumberIndex = i;
+        }
+      }
     }
+    arrNumTmp[numSmallestNumberIndex] = undefined;
+    arrPointer[numSmallestNumberIndex]++;
+    arrRes.push(numSmallest);
+    numSmallest = undefined;
+  }
 }
 
 // console.log(func3([[1, 2, 3, 6, 8, 9, 11, 13, 15, 23, 25, 27, 29, 34, 43], [3, 6, 8, 11, 12, 16, 23, 24, 34, 56, 66, 67], [4, 6, 8, 11, 14, 21, 24, 34, 35, 36, 41, 46]]))
-
--- undefined/2022-08-11.js
+++ undefined/2022-08-11.js
@@ -16,119 +16,121 @@ console.log(flat(arr))*/
 // findName(20, tree)，返回“济南”
 // findName(212, tree), 返回“烟台2”
 const tree = [
-    {
-        id: 1,
-        name: '浙江',
+  {
+    id: 1,
+    name: '浙江',
+    children: [
+      {
+        id: 10,
+        name: '杭州',
         children: [
-            {
-                id: 10,
-                name: '杭州',
-                children: [
-                    {
-                        id: 100,
-                        name: '西湖',
-                    },
-                    {
-                        id: 101,
-                        name: '余杭',
-                    },
-                ],
-            },
-            {
-                id: 11,
-                name: '绍兴',
-                children: [
-                    {
-                        id: 111,
-                        name: '绍兴1',
-                    },
-                    {
-                        id: 112,
-                        name: '绍兴2',
-                    },
-                ],
-            }
+          {
+            id: 100,
+            name: '西湖',
+          },
+          {
+            id: 101,
+            name: '余杭',
+          },
         ],
-    },
-    {
-        id: 2,
-        name: '山东',
+      },
+      {
+        id: 11,
+        name: '绍兴',
         children: [
-            {
-                id: 20,
-                name: '济南',
-                children: [
-                    {
-                        id: 200,
-                        name: '济南1',
-                    },
-                    {
-                        id: 201,
-                        name: '济南2',
-                    },
-                ],
-            },
-            {
-                id: 21,
-                name: '烟台',
-                children: [
-                    {
-                        id: 211,
-                        name: '烟台1',
-                    },
-                    {
-                        id: 212,
-                        name: '烟台2',
-                    },
-                ],
-            }
+          {
+            id: 111,
+            name: '绍兴1',
+          },
+          {
+            id: 112,
+            name: '绍兴2',
+          },
         ],
-    },
-    {
-        id: 3,
-        name: '澳门',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: '山东',
+    children: [
+      {
+        id: 20,
+        name: '济南',
         children: [
-            {
-                id: 30,
-                name: '澳门1'
-            },
-            {
-                id: 31,
-                name: '澳门2'
-            }
+          {
+            id: 200,
+            name: '济南1',
+          },
+          {
+            id: 201,
+            name: '济南2',
+          },
         ],
-    }
-]
+      },
+      {
+        id: 21,
+        name: '烟台',
+        children: [
+          {
+            id: 211,
+            name: '烟台1',
+          },
+          {
+            id: 212,
+            name: '烟台2',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: '澳门',
+    children: [
+      {
+        id: 30,
+        name: '澳门1',
+      },
+      {
+        id: 31,
+        name: '澳门2',
+      },
+    ],
+  },
+];
 
 function findName(id, tree) {
-    const strId = String(id)
-    let objTmp = tree[strId[0] - 1], res = objTmp.name
-    objTmp = objTmp.children
-    for (let i = 1; i < strId.length; i++) {
-        let index = strId[i]
-        if (!objTmp[index]) index -= 1
-        const {name, children} = objTmp[index]
-        objTmp = children
-        res = name
+  const strId = String(id);
+  for (let i = 0; i < strId.length; i++) {
+    const char = strId.slice(0, i + 1);
+    tree = tree.find(({ id }) => id === Number(char));
+    if (!tree) {
+      return undefined;
     }
-    return res
+    if (i === strId.length - 1) {
+      return tree.name;
+    } else {
+      tree = tree.children;
+    }
+  }
 }
 
-// console.log(findName(1, tree))
-// console.log(findName(20, tree))
-// console.log(findName(212, tree))
+console.log(findName(1, tree));
+console.log(findName(20, tree));
+console.log(findName(212, tree));
 
-const sleep = time => {
-    return new Promise(resolve => setTimeout(resolve, time))
-}
+const sleep = (time) => {
+  return new Promise((resolve) => setTimeout(resolve, time));
+};
 // sleep(1000).then(() => {
 //     console.log(1)
 // })
 
 async function func() {
-    console.log('hello')
-    await sleep(2000)
-    console.log('world')
+  console.log('hello');
+  await sleep(2000);
+  console.log('world');
 }
 
 // func()
@@ -139,47 +141,50 @@ async function func() {
 //
 // }
 
-
 function mergeObjectListByKey(oldList, newList, key) {
-    const arrRes = [], objRef = {}
-    for (let i = 0; i < oldList.length; i++) {
-        const list = oldList[i]
-        const keyVal = list[key]
-        objRef[keyVal] = list
-        arrRes.push(list)
-    }
-    for (let i = 0; i < newList.length; i++) {
-        const list = newList[i]
-        const keyVal = list[key]
-        if (!objRef.hasOwnProperty(keyVal)) arrRes.push(list)
-        else {
-            /*const objTmp = {
+  const arrRes = [],
+    objRef = {};
+  for (let i = 0; i < oldList.length; i++) {
+    const list = oldList[i];
+    const keyVal = list[key];
+    objRef[keyVal] = list;
+    arrRes.push(list);
+  }
+  for (let i = 0; i < newList.length; i++) {
+    const list = newList[i];
+    const keyVal = list[key];
+    if (!objRef.hasOwnProperty(keyVal)) arrRes.push(list);
+    else {
+      /*const objTmp = {
                 [key]: keyVal
             }*/
-            Object.keys(list).forEach((k) => {
-                const oldVal = objRef[keyVal][k], newVal = list[k]
-                if (oldVal !== newVal) {
-                    if (oldVal) delete objRef[keyVal][k]
-                    else objRef[keyVal][k] = newVal
-                }
-            })
+      Object.keys(list).forEach((k) => {
+        const oldVal = objRef[keyVal][k],
+          newVal = list[k];
+        if (oldVal !== newVal) {
+          if (oldVal) delete objRef[keyVal][k];
+          else objRef[keyVal][k] = newVal;
         }
+      });
     }
-    return arrRes
+  }
+  return arrRes;
 }
 // 测试数据
 const team = [
-    {id: 1, name: '张三', age: 11},
-    {id: 2, name: 'mike', age: 22},
-    {id: 3, name: '小李', age: 44},
-    {id: 5, name: '李', age: 44, gender: 1},
-]
+  { id: 1, name: '张三', age: 11 },
+  { id: 2, name: 'mike', age: 22 },
+  { id: 3, name: '小李', age: 44 },
+  { id: 5, name: '李', age: 44, gender: 1 },
+];
 const newTeam = [
-    {id: 2, name: 'mike', age: 99},
-    {id: 3, name: '小李', tel: 18899998888},
-    {id: 4, name: 'jay'},
-    {id: 5, name: '李', age: 44, gender: 0, tel: 12312312312},
-]
+  { id: 2, name: 'mike', age: 99 },
+  { id: 3, name: '小李', tel: 18899998888 },
+  { id: 4, name: 'jay' },
+  { id: 5, name: '李', age: 44, gender: 0, tel: 12312312312 },
+];
 
-const mergedTeam = mergeObjectListByKey(team, newTeam, 'id').sort((a, b) => a.id - b.id)
-console.log(mergedTeam)
+const mergedTeam = mergeObjectListByKey(team, newTeam, 'id').sort(
+  (a, b) => a.id - b.id
+);
+// console.log(mergedTeam);
